58cfb74b18f35e405d92b816dfb5c2256310d3b1
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-10-31 12:09:22 +0530

    add sample data and crop ccode automatically

---
+++ assets/js/crop.js
@@ -0,0 +1,51 @@
+function generateCropCode() {
+    const prefix = "C-";
+    const randomCode = Math.floor(1000 + Math.random() * 9000);
+    return prefix + randomCode;
+  }
+  
+  function setCropCode() {
+    const cropCodeInput = document.getElementById("cropCode");
+    cropCodeInput.value = generateCropCode();
+  }
+  
+  window.onload = function () {
+    setCropCode();
+  };
+  
+  document.getElementById("clearBtn").addEventListener("click", function () {
+    setCropCode();
+    document.getElementById("cropForm").reset();
+  });
+  
+  document.getElementById("saveBtn").addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Save functionality for the crop goes here");
+  });
+  
+  document.getElementById("updateBtn").addEventListener("click", function () {
+    alert("Update functionality for the crop goes here");
+  });
+  
+  document.getElementById("deleteBtn").addEventListener("click", function () {
+    alert("Delete functionality for the crop goes here");
+  });
+  
+  // Get all crops
+  document.getElementById("getAllBtn").addEventListener("click", function () {
+    const cropTableBody = document.getElementById("cropTableBody");
+    cropTableBody.innerHTML = `
+      <tr>
+        <td>C-1001</td><td>Rice</td><td>Oryza sativa</td><td>Cereal</td><td>Kharif</td><td>Staff 1</td><td>Requires ample water</td>
+      </tr>
+      <tr>
+        <td>C-1002</td><td>Cowpea</td><td>Vigna unguiculata</td><td>Legume</td><td>Rabi</td><td>Staff 2</td><td>N/A</td>
+      </tr>
+    `;
+  });
+  
+  document.getElementById("searchBtn").addEventListener("click", function () {
+    const cropCode = document.getElementById("searchCrop").value;
+    alert("Search functionality for crop code " + cropCode + " goes here");
+  });
+  
