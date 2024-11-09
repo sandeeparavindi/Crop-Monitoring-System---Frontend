354723fca1219df0c9b05451fa112b944c5c5b1a
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-09 17:42:27 +0530

    feild details table add images

--- assets/js/field-list.js
+++ assets/js/field-list.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
-    $.ajax({
-      url: "http://localhost:5050/cropMonitoring/api/v1/fields/allFields",
-      method: "GET",
-      success: function (data) {
-        data.forEach((field, index) => {
-          $("#cropDetailsTable").append(`
+  $.ajax({
+    url: "http://localhost:5050/cropMonitoring/api/v1/fields/allFields",
+    method: "GET",
+    success: function (data) {
+      data.forEach((field, index) => {
+        const fieldImage1 = field.fieldImage1
+          ? `<img src="data:image/jpeg;base64,${field.fieldImage1}" alt="Field Image 1" style="width: 50px; height: 50px; object-fit: cover;" />`
+          : "No Image";
+        const fieldImage2 = field.fieldImage2
+          ? `<img src="data:image/jpeg;base64,${field.fieldImage2}" alt="Field Image 2" style="width: 50px; height: 50px; object-fit: cover;" />`
+          : "No Image";
+
+        $("#cropDetailsTable").append(`
             <tr>
               <th scope="row">${index + 1}</th>
               <td>${field.fieldCode}</td>
@@ -13,17 +20,17 @@ $(document).ready(function () {
               <td>${field.extentSize}</td>
               <td>${field.crops}</td>
               <td>${field.staff}</td>
+              <td>${fieldImage1} ${fieldImage2}</td> <!-- Images Column -->
             </tr>
           `);
-        });
-      },
-      error: function (error) {
-        console.error("Error fetching field data:", error);
-        // alert('Could not fetch field details.');
-      },
-    });
-
-    $("#backBtn").click(function () {
-      window.location.href = "field.html";
-    });
+      });
+    },
+    error: function (error) {
+      console.error("Error fetching field data:", error);
+    },
   });
+
+  $("#backBtn").click(function () {
+    window.location.href = "field.html";
+  });
+});
