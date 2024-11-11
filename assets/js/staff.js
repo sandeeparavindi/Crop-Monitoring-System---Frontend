cbf9a1f875aac6033414e240f0de16cc18647809
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-11 20:29:50 +0530

    staff get all navigate

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -13,36 +13,10 @@ function generateStaffId() {
     setStaffId();
   };
   
-  document.getElementById("clearBtn").addEventListener("click", function () {
-    setStaffId();
-    document.getElementById("staffForm").reset();
+//getall
+$(document).ready(function () {
+  $('#getAllBtn').click(function () {
+    window.location.href = 'staff-list.html'; 
   });
-  
-  document.getElementById("saveBtn").addEventListener("click", function (e) {
-    e.preventDefault();
-    alert("Save functionality for the staff goes here");
-  });
-  
-  document.getElementById("updateBtn").addEventListener("click", function () {
-    alert("Update functionality for the staff goes here");
-  });
-  
-  document.getElementById("deleteBtn").addEventListener("click", function () {
-    alert("Delete functionality for the staff goes here");
-  });
-  
-  // Get all staff
-  document.getElementById("getAllBtn").addEventListener("click", function () {
-    // const staffTableBody = document.getElementById("staffTableBody");
-    // staffTableBody.innerHTML = `
-    //   <tr>
-    //     <td>S-1001</td><td>John</td><td>Doe</td><td>Manager</td><td>Male</td><td>2023-01-01</td><td>1980-02-15</td><td>1234567890</td><td>john@example.com</td><td>Manager</td><td>Field 1</td><td>Vehicle 1</td>
-    //   </tr>
-    // `;
-  });
-  
-  document.getElementById("searchBtn").addEventListener("click", function () {
-    const staffId = document.getElementById("searchStaff").value;
-    alert("Search functionality for staff ID " + staffId + " goes here");
-  });
+});
   
