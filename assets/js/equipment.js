97e0cc0465beedc25fff05276ffacdfef5824148
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-15 21:50:11 +0530

    load all staff members for equipment

--- assets/js/equipment.js
+++ assets/js/equipment.js
@@ -1,33 +1,52 @@
 function generateEquipmentId() {
-    const prefix = "EQ-";
-    const randomId = Math.floor(1000 + Math.random() * 9000);
-    return prefix + randomId;
+  const prefix = "EQ-";
+  const randomId = Math.floor(1000 + Math.random() * 9000);
+  return prefix + randomId;
 }
 
 function setEquipmentId() {
-    const equipmentIdInput = document.getElementById("equipmentId");
-    equipmentIdInput.value = generateEquipmentId();
+  const equipmentIdInput = document.getElementById("equipmentId");
+  equipmentIdInput.value = generateEquipmentId();
 }
 
 window.onload = function () {
-    setEquipmentId();
-    loadFields();
+  setEquipmentId();
+  loadFields();
+  loadStaffs();
 };
 
 function loadFields() {
-    fetch("http://localhost:5050/cropMonitoring/api/v1/fields/allFields")
-      .then((response) => response.json())
-      .then((data) => {
-        const fieldSelect = document.getElementById("assignedField");
-        fieldSelect.innerHTML =
-          '<option value="" disabled selected>Select Field</option>';
-  
-        data.forEach((field) => {
-          const option = document.createElement("option");
-          option.value = field.fieldCode;
-          option.text = `${field.fieldCode} - ${field.fieldName}`;
-          fieldSelect.appendChild(option);
-        });
-      })
-      .catch((error) => console.error("Error loading fields:", error));
-  }
+  fetch("http://localhost:5050/cropMonitoring/api/v1/fields/allFields")
+    .then((response) => response.json())
+    .then((data) => {
+      const fieldSelect = document.getElementById("assignedField");
+      fieldSelect.innerHTML =
+        '<option value="" disabled selected>Select Field</option>';
+
+      data.forEach((field) => {
+        const option = document.createElement("option");
+        option.value = field.fieldCode;
+        option.text = `${field.fieldCode} - ${field.fieldName}`;
+        fieldSelect.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error loading fields:", error));
+}
+
+function loadStaffs() {
+  fetch("http://localhost:5050/cropMonitoring/api/v1/staff/allstaff")
+    .then((response) => response.json())
+    .then((data) => {
+      const staffSelect = document.getElementById("assignedStaff");
+      staffSelect.innerHTML =
+        '<option value="" disabled selected>Select Staff</option>';
+
+      data.forEach((staff) => {
+        const option = document.createElement("option");
+        option.value = staff.id;
+        option.text = `${staff.id} - ${staff.firstName}`;
+        staffSelect.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error loading staff members:", error));
+}
