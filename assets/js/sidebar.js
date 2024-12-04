8eba2a23d27b2501e14010022b2872accfa21be0
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-12-04 19:03:58 +0530

    side bar js link all pages

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,48 +1,48 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-      const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId),
-        bodypd = document.getElementById(bodyId),
-        headerpd = document.getElementById(headerId);
-  
-      if (toggle && nav && bodypd && headerpd) {
-        toggle.addEventListener("click", () => {
-          nav.classList.toggle("show");
-          toggle.classList.toggle("bx-x");
-          bodypd.classList.toggle("body-pd");
-          headerpd.classList.toggle("body-pd");
-        });
-      }
-    };
-  
-    showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-    const linkColor = document.querySelectorAll(".nav_link");
-  
-    function colorLink() {
-      if (linkColor) {
-        linkColor.forEach((l) => l.classList.remove("active"));
-        this.classList.add("active");
-      }
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
+
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener("click", () => {
+        nav.classList.toggle("show");
+        toggle.classList.toggle("bx-x");
+        bodypd.classList.toggle("body-pd");
+        headerpd.classList.toggle("body-pd");
+      });
     }
-    linkColor.forEach((l) => l.addEventListener("click", colorLink));
-  });
+  };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const signOutLink = document.getElementById("signout-link");
+  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+  const linkColor = document.querySelectorAll(".nav_link");
 
-  signOutLink.addEventListener("click", function (event) {
-    event.preventDefault();
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    }
+  }
+  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.closest("#sign-out-btn")) {
+    e.preventDefault();
+
     Swal.fire({
-      title: 'Are you sure?',
-      text: 'Do you really want to sign out?',
-      icon: 'warning',
+      title: "Are you sure?",
+      text: "You will be logged out.",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Yes, Sign out!',
-      cancelButtonText: 'Cancel',
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, sign out!",
     }).then((result) => {
       if (result.isConfirmed) {
-        window.location.href = "/index.html"; 
+        window.location.href = "/logout";
       }
     });
-  });
+  }
 });
